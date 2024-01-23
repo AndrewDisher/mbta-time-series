875248b0ffb005372e4f5cac8b913888703c45bf
AndrewDisher <ajdisher1998@gmail.com>
2024-01-23 10:49:26 -0500

    Updated dependencies file

--- dependencies.R
+++ dependencies.R
@@ -1,7 +1,17 @@
 # Do not run. This file simply allows renv to track every dependency for the project here. 
 
 library(dplyr)
-library(ggplot2)
+library(fable)
+library(fabletools)
+library(feasts)
 library(forecast)
+library(generics)
+library(ggplot2)
+library(gridExtra)
+library(gt)
+library(imputeTS)
 library(lubridate)
+library(MASS)
 library(roxygen2) # For documentation of custom functions
+library(tidyr)
+library(tsibble)
